8a4fa0ef80d3fd3285344ce31db6b73006fe06e7
Евгений <jenka.mosk@gmail.com>
2026-07-21 20:56:43 +0300

    Remove duplicate root function

--- app.py
+++ app.py
@@ -24,9 +24,6 @@ app = FastAPI(
 app.get("/", response_model=MessageResponse)
 
 
-async def root():
-    return {"message": "Добро пожаловать в Кулинарную книгу API!", "recipe_id": None}
-
 
 @app.get("/", response_model=MessageResponse)
 async def root():
